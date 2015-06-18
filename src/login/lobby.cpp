30f695b5206a85dbad48d05f0cd8a76218f24f3c
UnfortunateFruit <UnfortunateFruit@hotmail.com>
2015-06-18 10:20:04 +0100

    commit3

--- src/login/lobby.cpp
+++ src/login/lobby.cpp
@@ -16,7 +16,7 @@
   You should have received a copy of the GNU General Public License
   along with this program.  If not, see http://www.gnu.org/licenses/
 
-  This file is part of DarkStar-server source code.
+  This file is part of Darkstar-server source code.
 
 ===========================================================================
 */
